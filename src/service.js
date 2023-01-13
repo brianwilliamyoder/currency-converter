d5609b75b754a52664f84063f41b56fa27125b6f
Brian Yoder <brianyoder@gmail.com>
2023-01-13 12:51:38 -0800

    create CurrencyService service

--- src/service.js
+++ src/service.js
@@ -1,9 +1,16 @@
-/*export class AgeCalculator {
-  constructor(age) {
-    this.age = age;
-    this.planetAge = {};
-    this.yearsUntil = {};
-    this.yearsSince = {};
+export default class CurrencyService {
+  static async getRates() {
+    try {
+      const response =  await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const jsonResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonResponse;
+    } catch(error) {
+      return error;
+    }
   }
-
- */
+}
+ 
