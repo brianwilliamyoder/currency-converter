c09ec811ac034f0fc40c5b99809ad25c51675f3d
Brian Yoder <brianyoder@gmail.com>
2023-01-14 11:47:23 -0800

    add convertCurrency function

--- src/index.js
+++ src/index.js
@@ -7,26 +7,34 @@ import CurrencyService from './service.js';
 
 function getRates(amount, country) {
   const response = CurrencyService.getRates();
-  if (response.result) {
-    convertCurrency(amount, country);
+  if (response.conversion_rates) {
+    convertCurrency(amount, country, response);
   } else {
-    handleError(country)
+    printError(country, response);
   }
+}
+
+function convertCurrency(amount, country, response) {
+  let exchangeRate= response.conversionRates[`${country}`];
+  let convertedAmount = exchangeRate / amount;
+  printCurrency(convertedAmount, country);
 }
 //UI Logic
 
-function convertCurrency(amount, country) {
+function printCurrency(convertedAmount, country) {
+  document.querySelector('#showCurrency').innerText = `You will have ${convertedAmount} in ${country}'s currency`;
 }
 
-function handleError(country) {
+function printError(country, response) {
+  document.querySelector('#showCurrency').innerText = `There was an error processing the exchange rate for currency code ${country}: ${response}. Please try again`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
+  const amount = document.querySelector('#amount-usd').value;
+  document.querySelector('#amount-usd').value = null;
   const country = document.querySelector('#country').value;
-  document.querySelector('#location').value = null;
-  const amount = document.querySelector('#amount').value;
-  document.querySelector('#amount').value = null;
+  document.querySelector('#country').value = null;
   getRates(amount, country);
 }
 
