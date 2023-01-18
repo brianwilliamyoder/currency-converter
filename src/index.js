2eb0aa9a9924ad1fa310fe0460f74d2a76f9c00d
Brian Yoder <brianyoder@gmail.com>
2023-01-18 08:30:34 -0800

    add dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { currencyCodes } from './currency-codes.js';
 import CurrencyService from './service.js';
 
 //Business Logic
 
 async function getRates(amount, country) {
-  const response = await CurrencyService.getRates(country, amount);
+  const response = await CurrencyService.getRates();
   if  (!(`${country}` in response.conversion_rates) || isNaN(amount) || amount === null) {
-    printError(response, country);
+    printError(response, country, amount);
   } else {
     convertCurrency(amount, country, response);
   }
@@ -29,6 +30,17 @@ function printError(response, country) {
   document.querySelector('#showCurrency').innerText = `There was an error processing the exchange rate for currency code ${country}: ${response["error-type"]}. Please try again`;
 }
 
+/*function createDropDown() {
+  let dropDown = document.getElementById('dropDown');
+  let codes = currencyCodes;
+  codes.forEach(function(code) {
+    const item = document.createElement("item");
+    item.value = code[0];
+    item.text = code[0];
+    dropDown.appendChild(item);
+  }); 
+}*/
+
 function handleFormSubmission(event) {
   event.preventDefault();
   const amount = document.querySelector('#amount-usd').value;
@@ -40,4 +52,17 @@ function handleFormSubmission(event) {
 
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let dropDown = document.getElementById('dropDown');
+  let codes = currencyCodes;
+  console.log(codes);
+  codes.forEach(function(code) {
+    const option = document.createElement("option");
+    option.value = code;
+    option.text = code;
+    dropDown.appendChild(option);
+  }); 
+  
 });
